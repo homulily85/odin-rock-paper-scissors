4aff2798d403a743d730bf7892e77486793fd844
homulily85 <157974079+homulily85@users.noreply.github.com>
2025-08-29 04:33:26 +0700

    Add UI to the game

--- scripts.js
+++ scripts.js
@@ -1,45 +1,92 @@
-let getComputerChoice = () => {
+let humanScore = 0
+let computerScore = 0
+let numberOfPlayedRound = 0
+let isEnded = false
+
+const getComputerChoice = () => {
     const choices = ["rock", "paper", "scissors"]
     const choice = Math.floor(Math.random() * 3)
     return choices[choice]
 }
-let getHumanChoice = () => {
-    let userChoice = prompt("Enter your choice!")
-    return userChoice
+const getHumanChoice = (button) => {
+    return button.innerText.toLowerCase()
 }
 
-let humanScore = 0
-let computerScore = 0
+const restartGame = () => {
+    humanScore = 0
+    computerScore = 0
+    numberOfPlayedRound = 0
+    isEnded = false
 
-function playRound(humanChoice, computerChoice) {
-    let humanChoiceNormal = humanChoice.toLowerCase()
-    console.log(`Human chose: ${humanChoiceNormal}`)
-    console.log(`Computer chose: ${computerChoice}`)
-    if ((humanChoiceNormal === "rock" && computerChoice === "paper") ||
-        (humanChoiceNormal === "paper" && computerChoice === "scissors") ||
-        (humanChoiceNormal === "scissors" && computerChoice === "rock")
+    let oldResultContainer = document.querySelector("#result")
+    if (oldResultContainer) oldResultContainer.remove()
+
+    let oldFinalResult = document.querySelector("#final-result")
+    if (oldFinalResult) oldFinalResult.remove()
+
+    let restartButton = document.querySelector("#restart")
+    if (restartButton) restartButton.remove()
+}
+
+const playRound = (humanChoice, computerChoice) => {
+    let winner = "No one"
+
+    if ((humanChoice === "rock" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "rock")
     ) {
-        console.log("Computer wins")
+        winner = "Computer"
         computerScore++
-    } else if (humanChoiceNormal === computerChoice) {
-        console.log("Draws")
+    } else if (humanChoice === computerChoice) {
     } else {
-        console.log("Human wins!")
+        winner = "Human"
         humanScore++
     }
 
-    console.log(`Current points\nHuman: ${humanScore}\nComputer: ${computerScore}`)
+    container = document.querySelector("#container")
 
+    let oldResultContainer = container.querySelector("#result")
+    if (oldResultContainer) {
+        container.removeChild(oldResultContainer)
+    }
+
+    let newResultContainer = document.createElement("div")
+    newResultContainer.setAttribute("id", "result")
+    newResultContainer.innerText = `Round ${++numberOfPlayedRound}\n
+                                Human chose: ${humanChoice}\n
+                                Computer chose: ${computerChoice}
+                                \n${winner} won\n
+                                Current point: Human ${humanScore} - ${computerScore} Computer`
+    container.append(newResultContainer)
 }
 
-for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice())
-}
+humanChoiceButtons = document.querySelectorAll(".human-choice")
+if (humanChoiceButtons.length > 0) {
+    humanChoiceButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (isEnded) {
+                return
+            }
+            if (computerScore < 5 && humanScore < 5) {
+                playRound(getHumanChoice(button), getComputerChoice())
+            }
 
-if (humanScore > computerScore) {
-    console.log("After 5 rounds, human wins!")
-} else if (humanScore < computerScore) {
-    console.log("After 5 rounds, computer wins!")
-} else {
-    console.log("After 5 rounds, no one wins!")
+            if (computerScore === 5 || humanScore === 5) {
+                let finalWinner = humanScore > computerScore ? "human" : "computer"
+                let container = document.querySelector("#container")
+
+                let finalResult = document.createElement("p")
+                finalResult.innerText = `After ${numberOfPlayedRound} rounds, ${finalWinner} won the game!`
+                finalResult.setAttribute("id", "final-result")
+                container.append(finalResult)
+
+                let restartButton = document.createElement("button")
+                restartButton.innerText = "New game"
+                restartButton.setAttribute("id", "restart")
+                restartButton.addEventListener("click", restartGame)
+                container.append(restartButton)
+                isEnded = true
+            }
+        })
+    });
 }
